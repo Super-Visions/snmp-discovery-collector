2a0895a901edac2af94172c7e5a7e382c783eb8f
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2024-01-31 10:43:10 +0100

    Update package version

--- module.snmp-discovery-collector.php
+++ module.snmp-discovery-collector.php
@@ -2,10 +2,11 @@
 /**
  * @copyright 2024 Super-Visions BVBA
  * @license http://opensource.org/licenses/AGPL-3.0
+ * @noinspection PhpUnhandledExceptionInspection
  */
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'snmp-discovery-collector/1.0.0',
+	'snmp-discovery-collector/1.1.0',
 	[]
 );
