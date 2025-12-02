906d5c74e2a014c79ec66e8f1003a89274359a50
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2025-12-02 17:29:10 +0100

    Fix worker

--- worker.php
+++ worker.php
@@ -11,6 +11,8 @@ require_once(APPROOT.'core/utils.class.inc.php');
 require_once(APPROOT.'core/restclient.class.inc.php');
 require_once(APPROOT.'core/collector.class.inc.php');
 require_once(APPROOT.'core/orchestrator.class.inc.php');
+require_once(APPROOT.'core/lookuptable.class.inc.php');
+require_once(APPROOT.'core/mappingtable.class.inc.php');
 require_once(__DIR__.'/main.php');
 
 try {
